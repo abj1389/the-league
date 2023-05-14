cace54dc0131446dae7fef119a6e0377be686179
abj1389 <abj_13@hotmail.com>
2023-05-14 17:57:30 +0200

    added validations in player model

--- models/Player.js
+++ models/Player.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const positions = ["PORTERO", "CIERRE", "ALA", "PIVOT"];
+
 const playerSchema = new Schema(
   {
     firstName: {
       type: String,
       required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 20,
     },
     lastName: {
       type: String,
       required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 20,
     },
     favPosition: {
       type: String,
       required: true,
+      trim: true,
+      enum: positions,
     },
     number: {
       type: Number,
       required: true,
+      min: 1,
+      max: 99,
     },
     team: {
       type: mongoose.Schema.Types.ObjectId,
